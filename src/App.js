d0e90810adabe4b3d4c0c4c69eca0fc04decaa8d
Sandis <sandissirmais8@gmail.com>
2021-11-30 14:36:30 +0200

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import React from "react";
+import Coin from "./components/coinItem/Coin";
+
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  
+
+  const handleChange = (e) => {
+    
+
+    setSearch(e.target.value);
+
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <div className="logoContainer">
+        <img className="mainLogo" src="./logo.png" alt="logo" />
+        <h1 className="brand">
+          cryptolv
+        </h1>
+        </div>
+        <form>
+        <div class="search-box">
+          <button class="btn-search"><i class="fas fa-search"></i></button>
+          <input type="text" onChange={handleChange} class="input-search" placeholder="Meklēt valūtu..."/>
+        </div>
+        </form>
+      </div>
+            <div className="coinsContainer">
+      {filteredCoins.map((coin) => {
+          return (
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              price={coin.current_price}
+              symbol={coin.symbol}
+              marketcap={coin.market_cap}
+              volume={coin.total_volume}
+              image={coin.image}
+              priceChange={coin.price_change_percentage_24h}
+            />
+          );
+        })}
+            </div>
     </div>
   );
 }
---
+++ src/components/coinItem/Coin.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react'
+import "./Coin.css"
+import Aos from "aos";
+import "aos/dist/aos.css"
+
+
+const Coin = ({name, price, symbol, marketcap, volume, image, priceChange}) => {
+        useEffect(() => {
+            Aos.init({duration: 1000})
+        }, []);
+            
+    return (
+        <div data-aos="fade-in" className="cryptoCoin">
+            <img src={image} alt={`${name}`} className="coinLogo"/>
+            <div className="coinNameWrap">
+                <h1 className="coinName">{name}</h1>
+                <p className="coinSymbol">{symbol}</p>
+            </div>
+            <p className="coinPrice">€{price.toLocaleString()}</p>
+            <p className="coinMarketCap">
+                Tirgus griesti: €{marketcap.toLocaleString()}
+            </p>
+            <p className="coinVolume">
+                Daudzums (24h): €{volume.toLocaleString()}
+            </p>
+            {priceChange < 0 ? (
+                <div className="priceContainerDOWN">
+                <i className="fas fa-angle-down fa-2x"></i>
+                <p className="priceChange">{priceChange.toFixed(2)}%</p>
+                </div>
+            ) : (
+                <div className="priceContainerUP">
+                <i className="fas fa-angle-up fa-2x"></i>
+                <p className="priceChange">{priceChange.toFixed(2)}%</p>
+                </div>
+            )
+        }
+        </div>
+    )
+}
+
+export default Coin
