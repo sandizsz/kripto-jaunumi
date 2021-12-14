01f8bc3b73b43cb8c0fa8f0cff0d70fba99a98b6
sandizsz <76697600+sandizsz@users.noreply.github.com>
2021-12-15 00:47:18 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
 
   const filteredCoins = coins.filter((coin) =>
     coin.name.toLowerCase().includes(search.toLowerCase()),
-    console.log(filteredCoins);
+    console.log(filteredCoins)
   );
 
   return (
